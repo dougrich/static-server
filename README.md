60623e09d39d0b4f9d3ce285571e3fe6bbe28d93
Douglas Richardson <dougrich@outlook.com>
2019-12-12 13:25:48 -0800

    Remove yosshi; outdated and had concurrent bug

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/dougrich/static-server/Publish Docker)
 
-Simple dockerized golang static server based on https://github.com/yosssi/go-fileserver
+Simple dockerized golang static server
 
 Mount files underneath `/static`
--- main.go
+++ main.go
@@ -1,16 +1,45 @@
 package main
 
+// based on https://gist.github.com/hauxe/f2ea1901216177ccf9550a1b8bd59178
+
 import (
+	"flag"
 	"log"
 	"net/http"
-
-	"github.com/yosssi/go-fileserver"
+	"strings"
 )
 
-func main() {
-	fs := fileserver.New(fileserver.Options{})
-	http.Handle("/", fs.Serve(http.Dir("/static")))
-	if err := http.ListenAndServe(":80", nil); err != nil {
-		log.Fatal("ListenAndServe: ", err)
+// FileSystem custom file system handler
+type FileSystem struct {
+	fs http.FileSystem
+}
+
+// Open opens file
+func (fs FileSystem) Open(path string) (http.File, error) {
+	f, err := fs.fs.Open(path)
+	if err != nil {
+		return nil, err
 	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := strings.TrimSuffix(path, "/") + "/index.html"
+		if _, err := fs.fs.Open(index); err != nil {
+			return nil, err
+		}
+	}
+
+	return f, nil
+}
+
+func main() {
+	port := flag.String("p", "3000", "port to serve on")
+	directory := flag.String("d", ".", "the directory of static file to host")
+	flag.Parse()
+
+	fileServer := http.FileServer(FileSystem{http.Dir(*directory)})
+	http.Handle("/statics/", http.StripPrefix(strings.TrimRight("/statics/", "/"), fileServer))
+
+	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
